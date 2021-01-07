6b98d9070053993a41eac81391137bcf0fee9b88
Ermek Barmashev <barmashev48@gmail.com>
2021-01-06 20:22:07 -0500

    added word capitalization :yum:

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const output = document.querySelector(".output");
 const paragraph = document.querySelector(".output");
 
 let numOfParagraphs = "";
-const numberOfWords = 80;
+let firstInSentence = "";
 
 const lovePhrases = [
   "chipotle mayo",
@@ -38,6 +38,33 @@ const lovePhrases = [
   "watermelons",
   "pouring beer over stones in sauna",
   "when a bartender remembers your drink",
+  "cherry beer",
+  "gin and tonic",
+  "cheap vodka",
+  "french language",
+  "sleeping on the fresh sheets",
+  "korean cinema",
+  "kazakh girls",
+  "smell of basement",
+  "shrimp",
+  "ikea",
+  "thrift stores",
+  "Manchester United",
+  "sad movies",
+  "taking roadtrips to nowhere in particular",
+  "helping people",
+  "Ancient Aliens",
+  "diversity",
+  "conspiracy theories",
+  "airplane food",
+  "havingg my expectations exceeded",
+  "black cats",
+  "girls with bob cut",
+  "swimming in the lake",
+  "making travel plans",
+  "travelling to random obscure places",
+  "getting roasted",
+  "getting tattoed",
 ];
 
 let outputText = ["Ermek ipsum"];
@@ -65,11 +92,18 @@ const evaluateArr = () => {
       const finishedWord = wordArray.join("");
       outputText.pop();
       outputText.push(finishedWord);
+    } else if (outputText.length % 5 === 1) {
+      const currentWord = outputText[outputText.length - 1];
+      const capitalizedWord = capitalize(currentWord);
+      outputText.pop();
+      outputText.push(capitalizedWord);
     }
   }
 
   if (paragraph) {
-    capitalize();
+    const firstWord = capitalize(outputText[0]);
+    outputText.shift();
+    outputText[0] = firstWord;
   }
 
   output.insertAdjacentHTML(
@@ -86,11 +120,8 @@ const addNewWord = () => {
   outputText.push(lovePhrases[randomIndex]);
 };
 
-const capitalize = () => {
-  const capitalizedWord =
-    outputText[0].charAt(0).toUpperCase() + outputText[0].slice(1);
-  outputText.shift();
-  outputText[0] = capitalizedWord;
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 const randomNumber = () => {
